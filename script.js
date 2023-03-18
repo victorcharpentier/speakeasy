cb740dea12db0d011144dca3b6e203f344a894d4
victorcharpentier <77386537+victorcharpentier@users.noreply.github.com>
2023-03-18 21:42:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ let isRecording = false;
 let speech = new SpeechSynthesisUtterance();
 let isSpeaking = false;
 
-const langSelect = document.getElementById('langSelect');
+const langSelect = document.getElementById("langSelect");
 let langCode = langSelect.value;
 
 // Mettre à jour la variable langCode à chaque changement de l'option sélectionnée
-langSelect.addEventListener('change', () => {
+langSelect.addEventListener("change", () => {
   langCode = langSelect.value;
 });
 
@@ -15,25 +15,26 @@ recognition.continuous = true;
 recognition.interimResults = true;
 
 recognition.onstart = () => {
-  console.log('Enregistrement en cours...');
-  document.getElementById('message').innerText = 'Enregistrement en cours...';
+  console.log("Enregistrement en cours...");
+  document.getElementById("message").innerText = "Enregistrement en cours...";
   isRecording = true;
 };
 
 recognition.onresult = (event) => {
-  let interimTranscript = '';
-  let finalTranscript = '';
+  let interimTranscript = "";
+  let finalTranscript = "";
 
   for (let i = event.resultIndex; i < event.results.length; i++) {
     const transcript = event.results[i][0].transcript;
     if (event.results[i].isFinal) {
-      finalTranscript += transcript + ' ';
+      finalTranscript += transcript + " ";
     } else {
       interimTranscript += transcript;
     }
   }
 
-  document.getElementById('resultat').innerHTML = finalTranscript + interimTranscript;
+  document.getElementById("resultat").innerHTML =
+    finalTranscript + interimTranscript;
 
   // Appel de l'API Deepl Translate
   const apiKey = "811ca348-1d0c-598e-0347-e9903ceaeea9:fx";
@@ -41,9 +42,10 @@ recognition.onresult = (event) => {
   const url = `${endpoint}?auth_key=${apiKey}&text=${finalTranscript}&target_lang=${langCode}`;
 
   fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      document.getElementById('resultatTraduit').innerHTML = data.translations[0].text;
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById("resultatTraduit").innerHTML =
+        data.translations[0].text;
       if (!isSpeaking) {
         speech.text = data.translations[0].text;
         speech.lang = langCode;
@@ -54,12 +56,12 @@ recognition.onresult = (event) => {
         };
       }
     })
-    .catch(error => console.log(error));
+    .catch((error) => console.log(error));
 };
 
 recognition.onend = () => {
-  console.log('Enregistrement terminé.');
-  document.getElementById('message').innerText = '';
+  console.log("Enregistrement terminé.");
+  document.getElementById("message").innerText = "";
   isRecording = false;
 };
 
@@ -71,8 +73,8 @@ const stopRecording = () => {
   recognition.stop();
 };
 
-document.getElementById('onoff').addEventListener('change', () => {
-  const on = document.getElementById('onoff').checked;
+document.getElementById("onoff").addEventListener("change", () => {
+  const on = document.getElementById("onoff").checked;
 
   if (on) {
     startRecording();
@@ -83,6 +85,6 @@ document.getElementById('onoff').addEventListener('change', () => {
   }
 });
 
-document.getElementById('language').addEventListener('change', () => {
-  langCode = document.getElementById('language').value.toUpperCase();
+document.getElementById("language").addEventListener("change", () => {
+  langCode = document.getElementById("language").value.toUpperCase();
 });
